6942441b04039b6de58e91f80b5976a520cdcbc6
beimnet777 <91521577+beimnet777@users.noreply.github.com>
2021-12-20 00:08:21 -0800

    hamburger menu and some media queries are added on the sitet

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -1,25 +1,34 @@
 function darkMode() {
   var css = document.getElementsByTagName("link")[1];
-  var button= document.getElementById("theme");
+  var button = document.getElementById("theme");
   if (css.getAttribute("href") == "") {
     css.setAttribute("href", "../CSS/dark.css");
-    button.src="../images/icons/output-onlinepngtools (5).png";
-    
+    button.src = "../images/icons/output-onlinepngtools (5).png";
+
   }
-  else{
-    css.setAttribute("href","");
-    button.src="../images/icons/output-onlinepngtools (6).png"
+  else {
+    css.setAttribute("href", "");
+    button.src = "../images/icons/output-onlinepngtools (6).png"
   }
 }
 function darkMode1() {
   var css = document.getElementsByTagName("link")[1];
-  var button= document.getElementById("theme");
+  var button = document.getElementById("theme");
   if (css.getAttribute("href") == "") {
     css.setAttribute("href", "../CSS/dark2.css");
-    button.src="../images/icons/output-onlinepngtools (5).png";
-    
+    button.src = "../images/icons/output-onlinepngtools (5).png";
+
   }
-  else{
-    css.setAttribute("href","");
-    button.src="../images/icons/output-onlinepngtools (6).png"
-  }}
+  else {
+    css.setAttribute("href", "");
+    button.src = "../images/icons/output-onlinepngtools (6).png"
+  }
+}
+const butn = document.getElementById("burger");
+const nav = document.getElementsByTagName("nav")[0]
+function toggler() {
+  butn.addEventListener("click", () => {
+    nav.classList.toggle("toggle");
+  });
+}
+toggler();
