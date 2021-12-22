1a4f28c88d8f6eb2f331a7ac929abf6f5b6b958a
beimnet777 <91521577+beimnet777@users.noreply.github.com>
2021-12-21 21:21:08 -0800

    directory modification on index.html

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -2,13 +2,13 @@ function darkMode() {
   var css = document.getElementsByTagName("link")[2];
   var button = document.getElementById("theme");
   if (css.getAttribute("href") == "") {
-    css.setAttribute("href", "../CSS/home.css");
-    button.src = "../images/icons/output-onlinepngtools (6).png";
+    css.setAttribute("href", "CSS/home.css");
+    button.src = "images/icons/output-onlinepngtools (6).png";
 
   }
   else {
     css.setAttribute("href", "");
-    button.src = "../images/icons/output-onlinepngtools (5).png"
+    button.src = "images/icons/output-onlinepngtools (5).png"
   }
 }
 function darkMode1() {
