151027a7d572dc900a994afe5b5cd53748d473f7
beimnet777 <91521577+beimnet777@users.noreply.github.com>
2021-12-22 06:15:16 -0800

    content is added in acadamics.html inistead of lorem and index page is changed to be in light theme by default

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -1,14 +1,14 @@
 function darkMode() {
-  var css = document.getElementsByTagName("link")[2];
+  var css = document.getElementsByTagName("link")[1];
   var button = document.getElementById("theme");
   if (css.getAttribute("href") == "") {
-    css.setAttribute("href", "CSS/home.css");
-    button.src = "images/icons/output-onlinepngtools (6).png";
+    css.setAttribute("href", "CSS/dark.css");
+    button.src = "images/icons/output-onlinepngtools (5).png";
 
   }
   else {
     css.setAttribute("href", "");
-    button.src = "images/icons/output-onlinepngtools (5).png"
+    button.src = "images/icons/output-onlinepngtools (6).png"
   }
 }
 function darkMode1() {
